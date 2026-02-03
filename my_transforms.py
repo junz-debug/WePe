51fafc02739a71f26a65ed6190104ef1f6b3d297
Nie Jun <80238048+junz-debug@users.noreply.github.com>
2026-02-03 16:28:13 +0800

    Add GaussianBlur and MaybeToTensor transformations

---
+++ my_transforms.py
@@ -0,0 +1,30 @@
+from typing import Sequence
+
+import torch
+from torchvision import transforms
+
+
+class GaussianBlur(transforms.RandomApply):
+    def __init__(self, *, p: float = 0.5, radius_min: float = 0.7, radius_max: float = 1.0):
+        keep_p = 1 - p
+        transform = transforms.GaussianBlur(kernel_size=9, sigma=(radius_min, radius_max))
+        super().__init__(transforms=[transform], p=keep_p)
+
+
+class MaybeToTensor(transforms.ToTensor):
+    def __call__(self, pic):
+        if isinstance(pic, torch.Tensor):
+            return pic
+        return super().__call__(pic)
+
+
+IMAGENET_DEFAULT_MEAN = (0.485, 0.456, 0.406)
+IMAGENET_DEFAULT_STD = (0.229, 0.224, 0.225)
+
+
+def make_normalize_transform(
+    mean: Sequence[float] = IMAGENET_DEFAULT_MEAN,
+    std: Sequence[float] = IMAGENET_DEFAULT_STD,
+) -> transforms.Normalize:
+    return transforms.Normalize(mean=mean, std=std)
+
